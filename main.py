d77d49081ba22cecab8f220e9182fd4eb536df4a
MichaelOli <michaeloliveira38@gmail.com>
2024-05-24 20:29:15 -0300

    versão 1.3

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ tarefas = []
 # Loop para adicionar tarefas
 while True:
     # Solicita o nome da tarefa
+    print(f'{'='*20} Adicionando tarefas {'='*20}')
     add_tarefa = input('Informe o nome de uma tarefa ou aperte "enter" para exibir a lista: ')
 
     # Verifica se o usuário inseriu uma tarefa ou deseja exibir a lista
